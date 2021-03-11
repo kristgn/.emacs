16d5602b503c6b731d51fb550bcaee4e803e00fb
kristgn <snawklakl@gmail.com>
2021-03-11 20:53:12 +0100

    Update packages

--- elpa/web-mode-20191102.1751/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "web-mode" "20191102.1751" "major mode for editing web templates" '((emacs "23.1")) :commit "2a881fe6ecdb0317bf553ff2f2f92ab4612d35d5" :keywords '("languages") :authors '(("François-Xavier Bois <fxbois AT Google Mail Service>")) :maintainer '("François-Xavier Bois") :url "http://web-mode.org")
